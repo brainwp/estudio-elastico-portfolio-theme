cfefb90b9de42bdd60a24e2142f93886918f4fbf
matheus <contato@matheusgimenez.com.br>
2014-12-02 16:13:05 -0200

    issue #3

--- loop/nav-single.php
+++ loop/nav-single.php
@@ -10,10 +10,10 @@
 
 <div class="article-nav <?php if( get_post_type() == 'post' || get_post_type() == 'team' ) echo 'blog'; ?>">
 	
-	<?php next_post_link('%link', "<i class='icon-angle-left'></i>"); ?>
+	<?php next_post_link('%link', "<i class='icon-angle-left'></i>".__('Proximo','other')); ?>
 	
 	<a href="<?php echo $home_url; ?>" class="close"><i class="icon-angle-right"></i><i class="icon-angle-left"></i></a>
 	
-	<?php previous_post_link('%link', "<i class='icon-angle-right'></i>" ); ?>
+	<?php previous_post_link('%link', __('Anterior','other')."<i class='icon-angle-right'></i>"); ?>
 	
 </div>
